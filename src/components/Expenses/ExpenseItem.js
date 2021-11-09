b17eb8e7cfda93e5e22b7d1614f21ce706c32a9e
Soliman <abdelrahman.lifestyle@gamil.com>
2021-11-09 21:57:56 +0200

    turning classes into arrow functions

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,9 +2,9 @@ import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
     return (
-        <Card  className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
